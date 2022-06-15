56f8153606a0faa3bef3dc7de550801c0071908e
Francisco Marin <franjmarin1991@gmail.com>
2022-06-14 22:52:58 -0400

    Phase 1 of the tutorial

---
+++ database/connectdb.js
@@ -0,0 +1,9 @@
+
+import mongoose from "mongoose";
+
+try {
+    mongoose.connect(process.env.URI_MONGO);
+    console.log("Connect DB ok 👋");
+} catch (error) {
+    console.log("Error de conexión a mongodb: " + error);
+}
