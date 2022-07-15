74f29aff379a31516235cdf09eb952da0bc5f691
Francisco Marin <franjmarin1991@gmail.com>
2022-07-15 17:01:22 -0400

    Add register,login logic. Create jwt at login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,50 @@
-export const register = (req, res) => {
-    console.log(req.body);
-    res.json({ ok: "Register" });
+import {User} from '../models/User.js'; 
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        
+        //alternativa buscando por email
+        let user = await User.findOne({email});
+        if (user) throw ({code: 11000});
+
+        user = new User({email, password});
+        await user.save();
+
+        //jwt token 
+
+        return res.status(201).json({ok: true});
+    } catch (error) {
+        console.log(error);
+        //alternativa por defecto mongoose
+        if (error.code === 11000) {
+            return res.status(400).json({error: "Ya existe este usuario"});
+        }
+        return res.status(500).json({error: "Error de servidor"});
+    }
 }
 
-export const login = (req, res) => {
-    res.json({ ok: "Login" });
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        let user = await User.findOne({email});
+        if (!user) return res.status(403).json({ error: "No existe este usuario"})
+        
+        const respuestaPassword = await user.comparePassword(password);
+        if (!respuestaPassword) return res.status(403).json({ error: "Contraseña incorrecta"});
+    
+        // Generar JWT token 
+        
+        const token = jwt.sign({uid: user._id}, process.env.JWT_SECRET);
+        
+        return res.json({ token });
+        
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({error: "Error de servidor"});
+    }
+    
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
-import Express from "express";
-import { login, register } from "../controllers/auth.controller.js"
+import {Router} from 'express';
 import { body } from 'express-validator';
+import { login, register } from "../controllers/auth.controller.js";
 import { validationResultExpress } from "../middlewares/validationResultExpress.js";
-const router = Express.Router();
+
+const router = Router();
 
 router.post('/register',
     [
